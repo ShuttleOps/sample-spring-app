81429b65dda244a94cecd47b7745ee6841da162d
Luu <lucas.giancola@live.com>
2020-05-04 11:28:01 -0400

    Added a custom landing page for shuttleops

--- src/main/java/org/indellient/samplespringapp/controllers/WelcomeController.java
+++ src/main/java/org/indellient/samplespringapp/controllers/WelcomeController.java
@@ -1,14 +1,16 @@
 package org.indellient.samplespringapp.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WelcomeController{
 	
 	@GetMapping("/")
 	public String index() {
-		return "This is sample-spring-app build and deployed by ShuttleOps!";
+		return "index";
 	}
 
 }
